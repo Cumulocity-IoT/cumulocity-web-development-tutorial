f75f181c848f32b320781a48c57d95722dc49a7f
Christian Guether <christian.guether@cumulocity.com>
2025-07-24 08:41:09 +0200

    update part 02

--- part-02/my-c8y-application/src/device-info/device-info.component.ts
+++ part-02/my-c8y-application/src/device-info/device-info.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, WritableSignal } from '@angular/core';
 import { DeviceDetails, TemperatureMeasuerement } from './device-info.model';
 import { DeviceInfoService } from './device-info.service';
+import { CoreModule } from '@c8y/ngx-components';
 
 @Component({
   selector: 'c8y-device-info',
   templateUrl: 'device-info.component.html',
+  imports: [CoreModule],
   providers: [DeviceInfoService],
 })
 export class DeviceInfoComponent implements OnInit {
